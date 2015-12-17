640d9a363a136d71b273284449fe5d672171910c
raymg32 <gershomraymundo@gmail.com>
2015-12-16 20:54:35 -0500

    Update file-ggr.java

--- file-ggr.java
+++ file-ggr.java
@@ -61,3 +61,67 @@ void scene(){
 //void keyPressed(){
 //}
 
+
+
+
+class Boat{
+  float dx,dy,speed;
+  
+  Boat (float tempDX, float tempDY,float tempSpeed){
+    dx = tempDX;
+    dy = tempDY;
+    speed = tempSpeed;
+  }
+
+  void display(){
+    stroke(0);
+    fill( 95, 68, 12);
+    rect( dx,dy, 80, 50);
+  }
+
+  void move(){
+    dx -= speed; 
+    if(dx == 0){
+    speed = speed * -1;
+    }
+    else if( dx == width){
+     speed = speed * -1;
+    }
+  }
+}
+
+
+
+class Squid {
+  float r, g, b;
+ private float xpos;
+ private float ypos;
+  float speed;
+  
+  Squid( float tempR, float tempG, float tempB, float tempXpos, float tempYpos, float tempspeed){
+    r = tempR;
+    g = tempG;
+    b = tempB;
+    xpos = tempXpos;
+    ypos = tempYpos;
+    speed = tempspeed;
+    
+  }
+  
+  void display(){
+    stroke(0);
+    fill( r, g, b);
+    ellipse( xpos, ypos, 60,60);
+  }
+
+void move(){
+  ypos -= speed;
+  if(ypos == horizon){
+    speed = speed * -1;
+    }
+    else if( ypos == height){
+     speed = speed * -1;
+    } 
+  
+  }
+}
